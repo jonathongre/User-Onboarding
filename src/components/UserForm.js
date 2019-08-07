3c3442d3552d46037a48ba245bdf1f1614803586
Jonathon Green <ericg9811@gmail.com>
2019-08-07 14:58:16 -0500

    Completed: Step 4-Display users to screen

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, Field, withFormik } from 'formik';
+import { Form, Field, withFormik, setNestedObjectValues } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = ({ values, errors, touched }) => {
+const UserForm = ({ values, errors, touched, status }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status])
+        }
+    }, [status]);
+
     return (
+        <>
         <div>
             <Form>
                 <label>
-                Full Name
-                {touched.name&& errors.name && <p>{errors.name}</p>}
-                <Field type='text' name='name' placeholder='Name' />
+                    Full Name
+                    {touched.name&& errors.name && <p>{errors.name}</p>}
+                    <Field type='text' name='name' placeholder='Name' />
                 </label>
                 <label>
-                Email
-                {touched.email && errors.email && <p>{errors.email}</p>}
-                <Field type='email' name='email' placeholder='Email' />
+                    Email
+                    {touched.email && errors.email && <p>{errors.email}</p>}
+                    <Field type='email' name='email' placeholder='Email' />
                 </label>
                 <label>
-                Password
-                {touched.password && errors.password && <p>{errors.password}</p>}
-                <Field type='password' name='password' placeholder='Password' />
-                </label>
+                    Password
+                    {touched.password && errors.password && <p>{errors.password}</p>}
+                    <Field type='password' name='password' placeholder='Password' />
+                    </label>
                 <label>
                     <Field type='checkbox' name='tos' checked={values.tos} />
                     Accept Terms of Service
@@ -29,16 +37,29 @@ const UserForm = ({ values, errors, touched }) => {
                 <button type='submit'>Submit</button>
             </Form>
         </div>
+
+        <div>
+            <h1>Users</h1>
+            {users
+                ? users.map(user => (
+                    <p key={user.id}>
+                    Name: {user.name}
+                    </p>
+                ))
+            : null}
+        </div>
+        </>
     )
 }
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({ name, email, password, tos}) {
+    mapPropsToValues({ name, email, password, tos, users }) {
         return {
             name: name || '',
             email: email || '',
             password: password || '',
-            tos: tos || false
+            tos: tos || false,
+            users: users || ''
         };
     },
 
@@ -54,7 +75,7 @@ const FormikUserForm = withFormik({
         .required('Password is required')
     }),
 
-    handleSubmit(values, { resetForm })  {
+    handleSubmit(values, { resetForm, setStatus })  {
         axios
         .post('https://reqres.in/api/users', {
             name: values.name,
@@ -64,6 +85,7 @@ const FormikUserForm = withFormik({
         })
         .then(response  => { 
             console.log(response.data)
+            setStatus(response.data)
         })
         .catch(error => {
             console.log(error);
